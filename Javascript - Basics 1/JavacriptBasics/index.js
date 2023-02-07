b1ea793913656d2a1ac676f2e3daab7054f223db
Turwash Chakraborty <121122397+Turwash@users.noreply.github.com>
2023-02-07 22:33:16 +0530

    Add files via upload

---
+++ Javascript - Basics 1/JavacriptBasics/index.js
@@ -0,0 +1,44 @@
+//console.log is used to print in javascript
+
+console.log("Namaste Duniyaaa...");
+
+// (1) Variable : 
+
+let b = 5;
+// let a = 'Turwash'
+// let a = true;
+// let a = 12.4
+console.log(a); 
+
+var a = 5;
+var a = 5; //redeclaration in var 
+console.log(a);
+
+const co = 12;
+console.log(co); // fixed const
+
+// (2) Dynamic typing in javascript : 
+
+let lastName = 'Turwash' ;
+console.log(lastName);
+lastName = 5; // data type changed
+console.log(lastName); 
+
+// (3) OPERATORS : _
+// ARITHMETIC OPERATOR :-
+
+let num = 4;
+let num1 = 2;
+console.log(num - num1);
+let num2 = 4 ** 4; // 4 to the power 4 
+console.log(num1)
+// similaryly for ( - , / , % , * )
+
+// (4) Pre Post Increment
+
+let x = 6;
+console.log(++x); //this will print 7
+
+let y = 6;
+console.log(y++); // this will print 6 but then update value of y to 7 
+console.log(y); // here its 7
