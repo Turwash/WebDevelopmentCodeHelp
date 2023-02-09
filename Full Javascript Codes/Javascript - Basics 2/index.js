051a97b0af99cdf16fdc631ce51db20a9b10f21e
Turwash Chakraborty <121122397+Turwash@users.noreply.github.com>
2023-02-09 21:24:10 +0530

    Add files via upload

---
+++ Full Javascript Codes/Javascript - Basics 2/index.js
@@ -0,0 +1,14 @@
+console.log("Lets Start");
+
+let rectangle = {
+    length : 2 ,
+    breadth : 4,
+
+    draw : function() {
+        console.log("Draw function is here")
+    }
+}
+
+
+console.log(reactangle.length);
+console.log(rectangle.draw());
