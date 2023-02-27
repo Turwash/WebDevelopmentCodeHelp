b03a2ad9118252a0520808f2338ed518e2af871c
Turwash Chakraborty <121122397+Turwash@users.noreply.github.com>
2023-02-27 18:33:36 +0530

    Add files via upload

---
+++ Homeworks And Projects/CounterApp/script.js
@@ -0,0 +1,20 @@
+const countValue = document.getElementById('counter');
+
+function increment(){
+    // get the vlue from UI
+    let value = parseInt(countValue.innerText);
+    //Update the value
+    value = value + 1;
+    //set the value onto UI
+    countValue.innerText = value;
+};
+
+
+function decrement(){
+        // get the vlue from UI
+        let value = parseInt(countValue.innerText);
+        //Update the value
+        value = value - 1;
+        //set the value onto UI
+        countValue.innerText = value;
+};
